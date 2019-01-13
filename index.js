ec861579063e7e2b0a47f49271feb6d7c46c597a
jameslock86 <31215180+jameslock86@users.noreply.github.com>
2019-01-12 23:46:40 -0600

    adding a question

--- index.js
+++ index.js
@@ -105,3 +105,25 @@ function gratestNumberOfString(arr){
 function fibonacci(n){
 
 }
+
+
+
+// whiteboard questions about variables and scope-->
+
+
+(function () {
+    var a = b = 3;
+})();
+
+console.log("a defined? " + (typeof a !== 'undefined'));
+console.log("b defined? " + (typeof b !== 'undefined'));
+
+// But how can b be defined outside of the scope of the enclosing function?W
+// ell, since the statement var a = b = 3; is shorthand for the statements b = 3;
+//  and var a = b;, b ends up being a global variable(since it is not preceded by the var keyword) 
+//  and is therefore still in scope even outside of the enclosing function.
+
+// Note that, in strict mode(i.e., with use strict), the statement var a = b = 3; will generate 
+// a runtime error of ReferenceError: b is not defined, thereby avoiding any headfakes / bugs that 
+// might othewise result. (Yet another prime example of why you should use use strict as a matter of
+//      course in your code!)
