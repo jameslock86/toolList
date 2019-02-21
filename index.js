1b0a7a2ef390c2177f6256a7b2223a274ac18a86
james lockwood <jameslockwood@jamess-MacBook-Air.local>
2019-02-20 23:19:47 -0600

    adding a Array.protoype question

--- index.js
+++ index.js
@@ -283,3 +283,45 @@ user.sayHi();
 
 // Declares a variable User that references the function named "constructor".
 // Puts methods listed in the definition into User.prototype. Here, it includes sayHi and the constructor.
+
+
+
+
+
+// Write functions that do the following: 
+// even // [1,2,3,4,5].even() should return [2,4]
+// odd // [1,2,3,4,5].odd() should return [1,3,5]
+// under // [1,2,3,4,5].under(4) should return [1,2,3]
+// over // [1,2,3,4,5].over(4) should return [5]
+// inRange // [1,2,3,4,5].inRange(1,3) should return [1,2,3]
+
+// They should also work when used together, for example:
+
+// [1,2,18,19,20,21,22,30,40,50,100].even().inRange(18,30) // should return [18, 20, 22, 30]
+
+// And finally the filters should only accept integer values from an array, for example:
+
+// ["a", 1, "b", 300, "x", "q", 63, 122, 181, "z", 0.83, 0.11].even() // should return [300, 122]
+Array.prototype.isInt = function() {
+    return this.filter(function(n) { return (typeof(n)==='number') && (n% 1 === 0) });
+    }
+    
+    Array.prototype.even = function() {
+    return this.isInt().filter(function(n) { return n%2===0 });
+    }
+    
+    Array.prototype.odd = function() {
+    return this.isInt().filter(function(n) { return n%2!== 0 });
+    }
+    
+    Array.prototype.under = function(x) {
+    return this.isInt().filter(function(n) { return n < x });
+    }
+    
+    Array.prototype.over = function(x) {
+    return this.isInt().filter(function(n) { return n > x });
+    }
+    
+    Array.prototype.inRange = function(min, max) {
+    return this.isInt().filter(function(n) { return n >= min && n <= max });
+    }
